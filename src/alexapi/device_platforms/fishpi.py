1f110f5921927ecb48aeb5a584a8825971eb76b2
John Dowling <john.patrick.dowling@gmail.com>
2017-02-15 19:26:32 -0800

    Initial raspberrypi clone code

--- src/alexapi/device_platforms/fishpi.py
+++ src/alexapi/device_platforms/fishpi.py
@@ -1,1 +1,16 @@
+import RPi.GPIO as GPIO
 
+from rpilike import RPiLikePlatform
+
+
+class FishpiPlatform(RPiLikePlatform):
+
+	def __init__(self, config):
+		super(RaspberrypiPlatform, self).__init__(config, 'fishpi', GPIO)
+
+	def setup(self):
+		GPIO.setwarnings(False)
+		GPIO.cleanup()
+		GPIO.setmode(GPIO.BCM)
+
+		super(RaspberrypiPlatform, self).setup()
